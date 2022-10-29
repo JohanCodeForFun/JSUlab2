4fa079a1a9d72d14e26c11c4b808db9e7adee578
Dennis <112337016+TheProgrammer1000@users.noreply.github.com>
2022-10-29 14:42:20 +0200

    Create function to get weather-data and store it

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-alert("TJABBA");
-console.log("boysen nu kör vi");
+// Här får vi allt väderdata till en array "weatherData"
+let weatherData = JSON.parse(localStorage.getItem('weatherData'))
+console.log(weatherData)
+
+GetWeatherData()
+
+// Function för att hämta väder data
+async function GetWeatherData() {
+  let response = await fetch('https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=60.5&lon=11.0')
+
+  let result = await response.json();
+
+  let arrayResult = [];
+
+  // Loopar igenom arrayn för att lägga till object till arrayn-variabeln "arrayResult".
+  for(let i = 0; i < result.properties.timeseries.length; i++) {
+    arrayResult.push(result.properties.timeseries[i].data.instant.details)
+  }
+  localStorage.setItem('weatherData', JSON.stringify(arrayResult))
+}
