294ec053b718332d163948441f94af31b431b832
Dennis <112337016+TheProgrammer1000@users.noreply.github.com>
2022-11-10 19:51:13 +0100

    CheckDateTime-function SOLVED, get all the data from index-page to fiskeplatser-page. inclued with nights the user picks too

--- js/index.js
+++ js/index.js
@@ -36,23 +36,24 @@ let guideElement = document.querySelector('#guide')
 
 // 2. Hämta data från user till annan sida
 
-let countercounterValue = document.querySelector("#counterValue"); //Bestämmer vilket id
-let decreaseButton = document.getElementById("decrease");
-counterValue.textContent = 1; //Bestämmer startvärdet
-decreaseButton.disabled = true;
-document.querySelector("#increase").addEventListener("click", () => {
-  counterValue.textContent++;
-  console.log(counterValue);
+let nightCount = document.querySelector('#nightCount')
+let counterValue = 1
 
-  if (counterValue.textContent > 0) {
-    decreaseButton.disabled = false;
-  }
+let decreaseButton = document.getElementById("decrease");
+decreaseButton.disabled = true;
+// counterValue.textContent = 1; //Bestämmer startvärdet
+document.querySelector("#increase").addEventListener("click", () => {
+    counterValue++;
+    nightCount.innerHTML = counterValue
+    if(counterValue > 1) {
+      decreaseButton.disabled = false;
+    }
 });
 
 document.querySelector("#decrease").addEventListener("click", () => {
-  counterValue.textContent--;
-  console.log(counterValue);
-  if (Number(counterValue.textContent) === 1) {
+  counterValue--;
+  nightCount.innerHTML = counterValue
+  if (counterValue === 1) {
     decreaseButton.disabled = true;
   }
 });
@@ -80,13 +81,10 @@ function CounterPrice() {
   for(let i = 0; i < vechileElement.length; i++) {
     let isChecked = vechileElement[i].checked
     if(isChecked === true) {
-      window.location.href = `http://127.0.0.1:5501/fiskeplatser.html?destination=${destinationElement.value}&startpoint=${startpointElement.value}&vechile=${vechileElement[i].value}&equipement=${equipementElement.value}&guide=${guideElement.value}`
+      window.location.href = `http://127.0.0.1:5501/fiskeplatser.html?destination=${destinationElement.value}&startpoint=${startpointElement.value}&vechile=${vechileElement[i].value}&equipement=${equipementElement.value}&guide=${guideElement.value}&night=${counterValue}`
     }
   }
 }
-
-
-
 
 // 3. Hämta väderdata funktioner
 // Header för att identifiera oss mot weather api, yr.no
@@ -119,6 +117,8 @@ async function GetAllWeatherData() {
     }
   }
 
+
+
   // Object.keys-funktion will return a array and locationKey is a array too
   // locationKeyInput är redan en array och Object.keys returnar också en array därför i detta fall använder jag mig av funktionen-flat som gör dessa till värden istället för arrays
   let locationKey = locationKeyInput.flat()
@@ -128,8 +128,6 @@ async function GetAllWeatherData() {
   for(let i = 0; i < result.length; i++) {
     weatherData.push({[locationKey[i]]: {data: [], updatedWeatherReport: {}}})
   }
-  // console.log(weatherData);
-
   // Genom denna loop får vi det uppdaterade-väder-raporten för just det specifika location
   for(let i = 0; i < result.length; i++) {
     let updatedDate = result[i][locationKey[i]].properties.meta.updated_at.slice(0,10)
@@ -172,124 +170,113 @@ async function GetAllWeatherData() {
   // SLUT PÅ FUNKTIONEN
 }
 
-// Function Jämnför den lokala tiden med tiden med väder-arrayn
-function checkDateTime() {
+function CheckDateTime() {
   let dateToday = new Date().toLocaleDateString();
   let timeToday = new Date().toLocaleTimeString();
 
   let weatherDataFinal = JSON.parse(localStorage.getItem('weatherDataFinal'))
   let locationKeys = JSON.parse(localStorage.getItem('locationKeys'))
+  locationKeys[1] = locationKeys[1].trim(); // Blev något skumt med index-1 att det var ett mellanrum på namnet
+
   // 3 deklarerade väder-iconer
   let weatherIcon = [];
 
   // Här så splitar jag varje tid på weatherData-arrayn och jämför om det är rätt datum och tid
-  console.log(weatherDataFinal.length)
   for(let i = 0; i < weatherDataFinal.length; i++) {
-    console.log(weatherDataFinal.locationKeys);
-    console.log(weatherDataFinal[1]);
-    console.log(weatherDataFinal[1][locationKeys[1]])
-    console.log(`Uncaught TypeError: Cannot read properties of null (reading '0') at checkDateTime (index.js:151:56)
-    at index.js:264:1`)
-    console.log(weatherDataFinal[0]);
     for(let j = 0; j < weatherDataFinal[i][locationKeys[i]].data.length; j++) {
-
       let ArrayindexTime = weatherDataFinal[i][locationKeys[i]].data[j].time.indexOf(":");
-
       let ArraytimeNow = weatherDataFinal[i][locationKeys[i]].data[j].time.slice(0, ArrayindexTime);
 
       let currentTimeIndex = timeToday.indexOf(":");
       let currentTime = timeToday.slice(0, currentTimeIndex);
 
-      // Kollar rätt tid för arrayn och för den lokala tiden
-      // Denna kommer ju bli sant 3 gånger, för vi har 3 olika locations
       if (ArraytimeNow === currentTime && weatherDataFinal[i][locationKeys[i]].data[j].date === dateToday) {
-        // Här kollar jag om våran array innehåller denna symbol och om det gör det så kollar vi också här i switchen vilken location det är. Sedan pushar vi in detta. Locationen kan bara vara sant 1 på i för vi har bara 3 location så tittar efter och så kan bara j också vara sant 1 gång för det är ju den nuvarande tiden så kommer va densamma. Därför blir det 3 gånger rätt på denna if-sats som vi sedan pushar in i arrayn-weatherIcon.
         switch(weatherDataFinal[i][locationKeys[i]].data[j]["Weather-Symbol"]) {
-        case "partlycloudy_night":
-          if(locationKeys[i] === "Tana") {
-            weatherIcon.push("partlycloudy_night")
-          }
-          else if(locationKeys[i] === "Lofoten ") {
-            weatherIcon.push("partlycloudy_night")
-          }
-          else if (locationKeys[i] === "Hallingdalselva"){
-            weatherIcon.push("partlycloudy_night")
-          }
-          break;
-        case "fair_night":
-          if(locationKeys[i] === "Tana") {
-            weatherIcon.push("fair_night")
-          }
-          else if(locationKeys[i] === "Lofoten ") {
-            weatherIcon.push("fair_night")
-          }
-          else if (locationKeys[i] === "Hallingdalselva"){
-            weatherIcon.push("fair_night")
-          }
-          break;
-        case "partlycloudy_day":
-          if(locationKeys[i] === "Tana") {
-            weatherIcon.push("partlycloudy_day")
-          }
-          else if(locationKeys[i] === "Lofoten ") {
-            weatherIcon.push("partlycloudy_day")
-          }
-          else if (locationKeys[i] === "Hallingdalselva"){
-            weatherIcon.push("partlycloudy_day")
-          }
-          break;
-        case "cloudy":
-          if(locationKeys[i] === "Tana") {
-            weatherIcon.push("cloudy")
-          }
-          else if(locationKeys[i] === "Lofoten ") {
-            weatherIcon.push("cloudy")
-          }
-          else if (locationKeys[i] === "Hallingdalselva"){
-            weatherIcon.push("cloudy")
-          }
-          break;
-        case "lightrain":
-          if(locationKeys[i] === "Tana") {
-            weatherIcon.push("lightrain")
-          }
-          else if(locationKeys[i] === "Lofoten ") {
-            weatherIcon.push("lightrain")
-          }
-          else if (locationKeys[i] === "Hallingdalselva"){
-            weatherIcon.push("lightrain")
-          }
-          break;
-        case "heavyrain":
-          if(locationKeys[i] === "Tana") {
-            weatherIcon.push("heavyrain")
-          }
-          else if(locationKeys[i] === "Lofoten ") {
-            weatherIcon.push("heavyrain")
-          }
-          else if (locationKeys[i] === "Hallingdalselva"){
-            weatherIcon.push("heavyrain")
-          }
-          break;
-        case "fog":
-          if(locationKeys[i] === "Tana") {
-            weatherIcon.push("fog")
-          }
-          else if(locationKeys[i] === "Lofoten ") {
-            weatherIcon.push("fog")
-          }
-          else if (locationKeys[i] === "Hallingdalselva"){
-            weatherIcon.push("fog")
-          }
-          break;
-        default:
-          weatherIcon.push("Missing weather data switch")
+          case "partlycloudy_night":
+            if(locationKeys[i] === "Tana") {
+              weatherIcon.push("partlycloudy_night")
+            }
+            else if(locationKeys[i] === "Lofoten") {
+              weatherIcon.push("partlycloudy_night")
+            }
+            else if (locationKeys[i] === "Hallingdalselva"){
+              weatherIcon.push("partlycloudy_night")
+            }
+            break;
+          case "fair_night":
+            if(locationKeys[i] === "Tana") {
+              weatherIcon.push("fair_night")
+            }
+            else if(locationKeys[i] === "Lofoten") {
+              weatherIcon.push("fair_night")
+            }
+            else if (locationKeys[i] === "Hallingdalselva"){
+              weatherIcon.push("fair_night")
+            }
+            break;
+          case "partlycloudy_day":
+            if(locationKeys[i] === "Tana") {
+              weatherIcon.push("partlycloudy_day")
+            }
+            else if(locationKeys[i] === "Lofoten") {
+              weatherIcon.push("partlycloudy_day")
+            }
+            else if (locationKeys[i] === "Hallingdalselva"){
+              weatherIcon.push("partlycloudy_day")
+            }
+            break;
+          case "cloudy":
+            if(locationKeys[i] === "Tana") {
+              weatherIcon.push("cloudy")
+            }
+            else if(locationKeys[i] === "Lofoten") {
+              weatherIcon.push("cloudy")
+            }
+            else if (locationKeys[i] === "Hallingdalselva"){
+              weatherIcon.push("cloudy")
+            }
+            break;
+          case "lightrain":
+            if(locationKeys[i] === "Tana") {
+              weatherIcon.push("lightrain")
+            }
+            else if(locationKeys[i] === "Lofoten") {
+              weatherIcon.push("lightrain")
+            }
+            else if (locationKeys[i] === "Hallingdalselva"){
+              weatherIcon.push("lightrain")
+            }
+            break;
+          case "heavyrain":
+            if(locationKeys[i] === "Tana") {
+              weatherIcon.push("heavyrain")
+            }
+            else if(locationKeys[i] === "Lofoten") {
+              weatherIcon.push("heavyrain")
+            }
+            else if (locationKeys[i] === "Hallingdalselva"){
+              weatherIcon.push("heavyrain")
+            }
+            break;
+          case "fog":
+            if(locationKeys[i] === "Tana") {
+              weatherIcon.push("fog")
+            }
+            else if(locationKeys[i] === "Lofoten") {
+              weatherIcon.push("fog")
+            }
+            else if (locationKeys[i] === "Hallingdalselva"){
+              weatherIcon.push("fog")
+            }
+            break;
+          default:
+            weatherIcon.push("Missing weather data switch")
         }
-       // För denna kommer endast kör 3 gånger för den exakta tiden finns bara 1 i arrayn och då på dessa 3 location och dessa element har jag redan i arrayn-locationElements
-      locationElements[i].innerHTML = `<h3>${weatherDataFinal[i][locationKeys[i]].data[j]["Weather-Details"].air_temperature} c°</h3>
-        <img src=img/${weatherIcon[i]}.png alt="Weather Icon" height="50px" width="50px">
-        `
-      break;
+         // För denna kommer endast kör 3 gånger för den exakta tiden finns bara 1 i arrayn och då på dessa 3 location och dessa element har jag redan i arrayn-locationElements
+        locationElements[i].innerHTML = `<h3>${weatherDataFinal[i][locationKeys[i]].data[j]["Weather-Details"].air_temperature} c°</h3>
+          <img src=img/${weatherIcon[i]}.png alt="Weather Icon" height="50px" width="50px">
+          `
+        break;
       }
     }
   }
@@ -300,11 +287,11 @@ function checkDateTime() {
 function IntervalLoop() {
   setInterval(() => {
     GetAllWeatherData()
-    checkDateTime()
+    CheckDateTime()
     }, 900000) //  900000 milisecounds, 15min interval update
 }
 GetAllWeatherData()   // Hämtar all väder-data och sorterar allting
-checkDateTime()       // Kollar vilket specifikt väder-objekt vi ska ta
+CheckDateTime()       // Kollar vilket specifikt väder-objekt vi ska ta
 IntervalLoop() // This function is asyncroumus
 
 // Display admin-uploads to home-page
